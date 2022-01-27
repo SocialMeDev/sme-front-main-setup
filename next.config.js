56f98b1136035ff8073b27e9b6d323c965bae21c
JoseVitor-1337 <josevitordemelo@yahoo.com>
2022-01-27 20:19:50 -0300

    Refactory: colocando as configurações corretas do ESLint/Prettier.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,12 @@
 const withPWA = require('next-pwa')
 
-module.exports = () => {
-	const isProduction = process.env.NODE_ENV === 'production'
-
-	const configs = {
-		trailingSlash: true,
-		reactStrictMode: true,
-		pwa: {
-			dest: 'public',
-			register: true,
-			skipWaiting: true,
-			disable: !isProduction
-		}
-	}
-
-	return withPWA(configs)
-}
+module.exports = withPWA({
+  trailingSlash: true,
+  reactStrictMode: true,
+  pwa: {
+    dest: 'public/pwa',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development'
+  }
+})
--- screens/index.js
+++ screens/index.js
@@ -3,20 +3,20 @@ import Lottie from 'react-lottie'
 import animationData from 'public/images/lotties/duck.json'
 
 export default function SetupPage() {
-	const defaultOptions = {
-		loop: true,
-		autoplay: true,
-		animationData,
-		rendererSettings: {
-			preserveAspectRatio: 'xMidYMid slice'
-		}
-	}
-	return (
-		<main>
-			<Center minH="100vh" flexDirection={'column'}>
-				<Lottie options={defaultOptions} height={300} width={300} />
-				<Heading>Setup Page</Heading>
-			</Center>
-		</main>
-	)
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  }
+  return (
+    <main>
+      <Center minH="100vh" flexDirection={'column'}>
+        <Lottie options={defaultOptions} height={300} width={300} />
+        <Heading>Setup Page</Heading>
+      </Center>
+    </main>
+  )
 }
