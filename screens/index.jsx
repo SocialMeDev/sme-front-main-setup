7c2a56be0c521b3283d2e360d4fa2db9c8a0f9e5
JoseVitor-1337 <josevitordemelo@yahoo.com>
2022-06-01 14:48:56 -0300

    Fix: corrigindo correções .

--- screens/index.jsx
+++ screens/index.jsx
@@ -1,20 +1,20 @@
-import { Center, Heading } from '@chakra-ui/react'
-import Lottie from 'react-lottie'
-import animationData from 'public/images/lotties/duck.json'
+import { Lottie, Center, Heading } from 'components'
+
+import duckAnimation from 'public/images/lotties/duck.json'
 
 export default function SetupPage() {
   const defaultOptions = {
     loop: true,
     autoplay: true,
-    animationData,
+    animationData: duckAnimation,
     rendererSettings: {
       preserveAspectRatio: 'xMidYMid slice'
     }
   }
   return (
     <main>
-      <Center minH="100vh" flexDirection={'column'}>
-        <Lottie options={defaultOptions} height={300} width={300} />
+      <Center minH="100vh" flexDirection="column">
+        <Lottie options={defaultOptions} size={300} />
         <Heading>Setup</Heading>
       </Center>
     </main>
