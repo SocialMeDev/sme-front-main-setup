a7a18193b115f11b549da781766a1937c7cd1ae6
JoseVitor-1337 <josevitordemelo@yahoo.com>
2022-07-11 15:52:46 -0300

    Feature: subindo so contextos padrões e carregamento do server side no _app.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,42 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import '@fontsource/poppins'
+
+import 'configs/styles'
+
+import { Loader } from 'components'
+import SocialMeProvider from 'contexts/SocialMe/Provider'
+import AuthProvider from 'contexts/Auth/Provider'
+import Theme from 'infrastructure/Theme'
 
 import theme from 'styles/theme'
 
-function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
+  const [loadingRoutes, setLoadingRoutes] = useState(true)
+  const { isReady } = useRouter()
+
+  useEffect(() => {
+    if (isReady) setLoadingRoutes(false)
+  }, [isReady])
+
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-      <ToastContainer
-        position="top-center"
-        autoClose={2000}
-        pauseOnHover={false}
-      />
-    </ChakraProvider>
+    <Theme theme={theme}>
+      {loadingRoutes ? (
+        <Loader text="Carregando" height="100vh" />
+      ) : (
+        <SocialMeProvider>
+          <AuthProvider>
+            <Component {...pageProps} />
+
+            <ToastContainer
+              position="top-center"
+              autoClose={2000}
+              pauseOnHover={false}
+            />
+          </AuthProvider>
+        </SocialMeProvider>
+      )}
+    </Theme>
   )
 }
-export default App
+export default MyApp
